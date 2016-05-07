2342199ffe2e634a8e17fe4db7793d64ef672ebc
David Castner <davidjcastner@gmail.com>
2016-05-07 18:55:05 -0400

    add account user profile

--- app/server/server.js
+++ app/server/server.js
@@ -5,3 +5,11 @@ Tasks = new Mongo.Collection("tasks");
 Meteor.publish("publicTasks", function () {
     return Tasks.find();
 });
+
+Accounts.onCreateUser(function (options, user) {
+    user.profile = options.profile;
+    user.profile.friends = [];
+    user.profile.score = 0;
+    user.profile.badges = [];
+    return user;
+})
