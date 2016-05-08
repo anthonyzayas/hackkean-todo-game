1903b20a3157b5ff8e93f05c4cd7f6f29a02b36c
Alexander-Huh <alexander707huh@gmail.com>
2016-05-07 22:39:51 -0400

    the profile page js file

---
+++ app/client/pages/profilePage.js
@@ -0,0 +1,15 @@
+Template.profilePage.helpers({
+
+  getName: function(){
+    return [];
+  }
+
+  getScore: function() {
+    return {{score}};
+  }
+
+  getBadge: function(){
+    return [];
+  }
+
+})
